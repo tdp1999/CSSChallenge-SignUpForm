f5df7edca4f935ac08d63956bf2aec4562974539
tdp1999 <tdp99.business@gmail.com>
2021-12-24 18:13:15 +0700

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'intro-component-with-signup-form-master';
+export class AppComponent implements OnInit {
+  form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+        ),
+      ],
+    ],
+    password: ['', Validators.required],
+  });
+
+  get firstName() {
+    return this.form.get('firstName') as FormControl;
+  }
+
+  get lastName() {
+    return this.form.get('lastName') as FormControl;
+  }
+
+  get email() {
+    return this.form.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.form.get('password') as FormControl;
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     extend: {
       colors: {
         primaryRed: "#FF7A7A",
+        primaryRedHover: "#ffa3a3",
         primaryGreen: "#38CC8C",
+        primaryGreenHover: "#63e0aa",
         accentBlue: "#6055A5",
+        accentBlueHover: "#8478cc",
         neutralDarkBlue: "#3E3C49",
         neutralGrayBlue: "#B9B6D3",
+      },
+      boxShadow: {
+        tryIt: "0px 10px 2px -2px rgba(0,0,0,0.3)",
+        tryItDes: "0px 10px 2px -5px rgba(0,0,0,0.3)",
       },
     },
   },
