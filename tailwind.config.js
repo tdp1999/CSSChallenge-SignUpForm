f3cdd26b302e46166b0cdd25e2ee6e766fea71eb
tdp1999 <tdp99.business@gmail.com>
2021-12-24 12:05:44 +0700

    setup code editor and general style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryRed: "#FF7A7A",
+        primaryGreen: "#38CC8C",
+        accentBlue: "#6055A5",
+        neutralDarkBlue: "#3E3C49",
+        neutralGrayBlue: "#B9B6D3",
+      },
+    },
   },
   plugins: [],
-}
+};
